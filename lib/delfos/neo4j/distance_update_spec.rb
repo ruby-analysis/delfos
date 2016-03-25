6587a0f7d5f804e28b652241568cf24c22cb5011
Mark Burns <markthedeveloper@gmail.com>
2016-03-25 14:18:58 +0000

    improve spec readability

--- lib/delfos/neo4j/distance_update_spec.rb
+++ lib/delfos/neo4j/distance_update_spec.rb
@@ -35,30 +35,23 @@ describe Delfos::Neo4j::DistanceUpdate do
     @result = ::Neo4j::Session.query(query)
   end
 
-  it do
-    props = @result.map(&:caller).map(&:props).uniq
-    expect(props.length).to eq 1
-  end
-
-  it do
-    props = @result.map(&:caller).map(&:props).uniq
-    prop = props.first
-    expect(prop[:file]).to match %r{fixtures/ruby/efferent_coupling\.rb}
-    expect(prop).to include({name: "lots_of_coupling", line_number: "5"})
-  end
-
-  it do
+  it "records the Classes" do
     klasses = @result.map(&:klass).map(&:labels).flatten.uniq
     expect(klasses).to eq [:EfferentCoupling]
   end
 
-  it do
+  it "records the called classes" do
     called_klasses = @result.map(&:called_klass).map(&:labels).flatten.uniq
     expect(called_klasses).to eq [:This, :That, :SomeOther, :SoMuchCoupling, :HereIsSomeMore]
   end
 
   describe "caller" do
-    it do
+    it "returns the caller"do
+      props = @result.map(&:caller).map(&:props).uniq
+      expect(props.length).to eq 1
+    end
+
+    it "records the caller details" do
       caller_props = @result.map(&:caller).map(&:props).flatten.uniq
 
       expect(caller_props).to eq [
@@ -69,7 +62,7 @@ describe Delfos::Neo4j::DistanceUpdate do
     end
   end
 
-  it do
+  it "records the called method details" do
     called_props = @result.map(&:called).map(&:props).flatten.uniq
 
     expect(called_props).to eq [
