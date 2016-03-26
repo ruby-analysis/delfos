418c40c3dfc9b97a6adfda40dc664c206e3cec40
Mark Burns <markthedeveloper@gmail.com>
2016-03-26 08:23:26 +0000

    refactor

--- lib/delfos/neo4j/distance_update.rb
+++ lib/delfos/neo4j/distance_update.rb
@@ -3,35 +3,53 @@ require_relative "../distance/calculation"
 module Delfos
   module Neo4j
     class DistanceUpdate
-
       def perform
         query = <<-QUERY
-          MATCH (klass)-[:OWNS]->(caller),
-            caller<-[:CALLED_BY]-(method_call),
-            method_call-[:CALLS]->(called),
-            (called_klass)-[:OWNS]->(called)
+          MATCH
+            (klass)         -  [:OWNS]      -> (caller),
+            (caller)       <-  [:CALLED_BY] -  (method_call),
+            (method_call)   -  [:CALLS]     -> (called),
+            (called_klass)  -  [:OWNS]      -> (called)
 
           RETURN klass, caller, method_call, called, called_klass
         QUERY
 
         result = ::Neo4j::Session.query(query)
 
+        update(result)
+      end
+
+      private
+
+      def update(result)
         result.each do |r|
           start  = determine_full_path r.caller.props[:file]
           finish = determine_full_path r.called.props[:file]
 
           calc = Delfos::Distance::Calculation.new(start, finish)
 
-          ::Neo4j::Session.query <<-QUERY
-            START caller = node(#{r.caller.neo_id}),
-                  called = node(#{r.called.neo_id})
-
-             MERGE caller - [:EFFERENT_COUPLING{distance:#{calc.sum_traversals}, possible_distance: #{calc.sum_possible_traversals}}] -> called
-          QUERY
+          perform_query(calc, r)
         end
       end
 
-      private
+      def perform_query(calc, r)
+        ::Neo4j::Session.query <<-QUERY
+          START caller = node(#{r.caller.neo_id}),
+                called = node(#{r.called.neo_id})
+
+           MERGE (caller) - #{rel_for(calc)} -> (called)
+        QUERY
+      end
+
+      def rel_for(calc)
+        <<-REL
+           [:EFFERENT_COUPLING{
+             distance:          #{calc.sum_traversals},
+             possible_distance: #{calc.sum_possible_traversals}
+             }
+           ]
+        REL
+      end
 
       def determine_full_path(f)
         f = Pathname.new File.expand_path f
@@ -42,8 +60,6 @@ module Delfos
           path if path.exist?
         end.compact.first
       end
-
-
     end
   end
 end
