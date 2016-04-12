b2309f5cc9e5bba07ee94e6c84723509af9a27c8
Mark Burns <markthedeveloper@gmail.com>
2016-04-12 22:12:49 +0100

    add spec for Delfos::Patching

--- lib/delfos/patching.rb
+++ lib/delfos/patching.rb
@@ -1,32 +1,74 @@
 # frozen_string_literal: true
 module Delfos
-  class Patching
-    def self.perform(klass, name, private_methods, class_method:)
-      new(klass, name, private_methods, class_method).setup
-    end
-
-    def self.added_methods
-      @added_methods ||= {}
-    end
-
-    def self.method_definition_for(klass, key)
-      super_klass = eval(klass).ancestors.detect do |k|
-        added_methods[k.to_s]
+  # This existence of these `Unstubbing` modules are an unfortunate side effect of trying to
+  # test something that redefines methods.  In order to not call the logging
+  # defined in `Delfos::Patching#setup` multiple times we have to keep track of
+  # and remove the method definitions and replace with the original definition
+  # (or as close to the original as possible).
+  #
+  # If there is a better way to test Delfos::Patching without doing this then
+  # please suggest or replace with a cleaner alternative
+  module Unstubbing
+    module ClassMethods
+      def add_instance_to_unstub!(object)
+        instances_to_unstub.push(object)
       end
 
-      klass_hash = added_methods[super_klass.to_s] || {}
-      klass_hash[key]
+      def unstub_all!
+        instances_to_unstub.each(&:unstub!)
+      end
+
+      private
+
+      def instances_to_unstub
+        @instances_to_unstub ||= []
+      end
+    end
+
+    module InstanceMethods
+      # See `Delfos::Patching#setup` to understand how this method is the
+      # inverse of that one.
+      def unstub!
+        original = original_method
+        class_method = class_method()
+        performer = method(:perform_call)
+
+        method_defining_method.call(name) do |*args, **keyword_args, &block|
+          method_to_call = class_method ? original : original.bind(self)
+          result = performer.call(method_to_call, args, keyword_args, block)
+          result
+        end
+      end
+    end
+  end
+
+  class Patching
+    extend Unstubbing::ClassMethods
+    include Unstubbing::InstanceMethods
+
+    class << self
+      def perform(klass, name, private_methods, class_method:)
+        new(klass, name, private_methods, class_method).setup
+      end
+
+      def added_methods
+        @added_methods ||= {}
+      end
+
+      def method_definition_for(klass, key)
+        # Find method definitions defined in klass or its ancestors
+        super_klass = eval(klass).ancestors.detect do |k|
+          added_methods[k.to_s]
+        end
+
+        klass_hash = added_methods[super_klass.to_s] || {}
+        klass_hash[key]
+      end
     end
 
     attr_reader :klass, :name, :private_methods, :class_method
 
-    def initialize(klass, name, private_methods, class_method)
-      @klass = klass
-      @name = name
-      @private_methods = private_methods
-      @class_method = class_method
-    end
-
+    # Redefine the method (only once) at runtime to enabling logging to Neo4j
     def setup
       return if ensure_method_recorded!
       original = original_method
@@ -43,6 +85,17 @@ module Delfos
       end
     end
 
+
+    def initialize(klass, name, private_methods, class_method)
+      @klass = klass
+      @name = name
+      @private_methods = private_methods
+      @class_method = class_method
+
+      self.class.add_instance_to_unstub!(self)
+    end
+    private_class_method :initialize
+
     private
 
     def perform_call(method_to_call, args, keyword_args, block)
@@ -53,27 +106,13 @@ module Delfos
       end
     end
 
-    def private_method?
-      private_methods.include?(name.to_sym)
-    end
-
-    def exclude?
-      ::Delfos::MethodLogging.exclude_from_logging?(original_method)
-    end
 
     def original_method
-      @original_methods ||= class_method ? klass.singleton_method(name) : klass.instance_method(name)
+      @original_method ||= class_method ? klass.singleton_method(name) : klass.instance_method(name)
     end
 
     def method_defining_method
       class_method ? klass.method(:define_singleton_method) : klass.method(:define_method)
-    end
-
-    def method_has_been_added?
-      return false unless self.class.added_methods[self]
-      return false unless self.class.added_methods[self][klass]
-
-      self.class.added_methods[klass][key]
     end
 
     def ensure_method_recorded!
@@ -87,6 +126,21 @@ module Delfos
 
     def bail?
       method_has_been_added? || private_method? || exclude?
+    end
+
+    def method_has_been_added?
+      return false unless self.class.added_methods[self]
+      return false unless self.class.added_methods[self][klass]
+
+      self.class.added_methods[klass][key]
+    end
+
+    def private_method?
+      private_methods.include?(name.to_sym)
+    end
+
+    def exclude?
+      ::Delfos::MethodLogging.exclude_from_logging?(original_method)
     end
 
     def key
---
+++ lib/delfos/patching_spec.rb
@@ -0,0 +1,60 @@
+require_relative "patching"
+
+describe Delfos::Patching do
+  class SomeAnonymousClass
+    def some_public_method
+      some_private_method
+    end
+
+    private
+
+    def some_private_method
+      "private"
+    end
+  end
+
+  describe ".perform" do
+    after do
+      described_class.unstub_all!
+    end
+
+    context "with a class with a private method" do
+      let(:klass) { SomeAnonymousClass }
+      let(:some_anonymous_instance) { klass.new }
+
+      before do
+        allow_any_instance_of(described_class).to receive(:exclude?) do
+          klass != SomeAnonymousClass
+        end
+
+        allow(Delfos::ExecutionChain).to receive(:pop)
+      end
+
+      it "includes public methods" do
+        expect(Delfos::MethodLogging).to receive(:log)
+        described_class.perform(klass, "some_public_method", klass.private_instance_methods, class_method: false)
+
+        some_anonymous_instance.some_public_method
+      end
+
+      it "excludes private methods" do
+        expect(Delfos::MethodLogging).not_to receive(:log)
+
+        described_class.perform(klass, "some_private_method", klass.private_instance_methods, class_method: false)
+
+        some_anonymous_instance.send(:some_private_method)
+      end
+
+      it "sends the correct args to the method logger" do
+        expect(Delfos::MethodLogging).to receive(:log) do |object, args, keyword_args, block, class_method, stack, call_site_binding, original_method|
+          expect(object).to be_a SomeAnonymousClass
+          expect(original_method.name).to eq :some_public_method
+        end
+
+        described_class.perform(klass, "some_public_method", klass.private_instance_methods, class_method: false)
+
+        some_anonymous_instance.some_public_method
+      end
+    end
+  end
+end
