837b38ad2b3fa01f0c8d947b7a3c5740c6466568
Mark Burns <markthedeveloper@gmail.com>
2016-05-12 06:26:48 +0900

    replace curl with Net::HTTP

--- lib/delfos/neo4j/query_execution.rb
+++ lib/delfos/neo4j/query_execution.rb
@@ -1,33 +1,59 @@
 # frozen_string_literal: true
 require "delfos"
 require "json"
+require "net/http"
+require "uri"
 
 module Delfos
   module Neo4j
     module QueryExecution
       class << self
         def execute(query, url=nil)
-          result = JSON.parse response_for(query)
+          result = response_for(query)
+          result = JSON.parse result.body
           result["results"].first["data"].map{|r| r["row"]}
         end
 
         private
 
         def response_for(query)
-          body = <<-BODY.gsub(/^\s+/, "").chomp
-            {"statements": [ { "statement": #{query.inspect} }]}
-          BODY
-          url = "http://neo4j:password@localhost:7474/db/data/transaction/commit"
+          request = request_for(query)
+          http = Net::HTTP.new(uri.host, uri.port)
 
-          command = "curl  #{headers} --data-ascii '#{body}' #{url} 2> /dev/null"
-
-          `#{command}`
+          http.request(request)
         end
 
-        def headers
-          <<-HEADERS.chomp
-            -H "User-Agent: delfos" -H "Accept: application/json; charset=UTF-8" -H "Content-Type: application/json" 
-          HEADERS
+        def request_for(query)
+          Net::HTTP::Post.new(uri.request_uri).tap do |request|
+            request.basic_auth(username, password)
+            request.content_type = 'application/json'
+
+            request.body = <<-BODY.gsub(/^\s+/, "").chomp
+              {"statements": [ { "statement": #{query.inspect} }]}
+            BODY
+          end
+        end
+
+        def uri
+          @uri ||= URI.parse "#{server_url}/db/data/transaction/commit"
+        end
+
+        def server_url
+          _, server_url, _ = Delfos.neo4j_config
+
+          server_url
+        end
+
+        def username
+          @username ||= options[:basic_auth][:username]
+        end
+
+        def password
+          @password ||= options[:basic_auth][:password]
+        end
+
+        def options
+          @options ||= Delfos.neo4j_config.last
         end
       end
     end
