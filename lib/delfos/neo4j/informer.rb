9543629911a8cfcb2f9976cb1b86e29d044b653b
Mark Burns <markthedeveloper@gmail.com>
2016-12-10 20:41:12 +0000

    parameterize queries

--- lib/delfos/neo4j/informer.rb
+++ lib/delfos/neo4j/informer.rb
@@ -11,28 +11,58 @@ module Delfos
 
       def execute_query(*args)
         query = query_for(*args)
-        params = {}
+        params = params_for(*args)
 
         QueryExecution.execute(query, params)
+      end
+
+      def params_for(args, call_site, called_code)
+        assign_query_variables(args, call_site, called_code)
+
+        params = query_variables.each_with_object({}) do |(klass, name), object|
+          object[name]=klass.to_s
+        end
+
+        params["m1_type"]        = call_site.method_type
+        params["m1_name"]        = call_site.method_name
+        params["m1_file"]        = call_site.method_definition_file
+        params["m1_line_number"] = call_site.method_definition_line
+
+        params["cs_file"]        = call_site.file
+        params["cs_line_number"] = call_site.line_number
+
+        params["m2_type"]        = called_code.method_type
+        params["m2_name"]        = called_code.method_name
+        params["m2_file"]        = called_code.method_definition_file
+        params["m2_line_number"] = called_code.method_definition_line
+        params
       end
 
       def query_for(args, call_site, called_code)
         assign_query_variables(args, call_site, called_code)
 
         klasses_query = query_variables.map do |klass, name|
-          "MERGE (#{name}:Class {name: #{klass.inspect}})"
+          "MERGE (#{name}:Class {name: {#{name}}})"
         end.join("\n")
 
         <<-QUERY
           #{klasses_query}
 
-          MERGE (#{query_variable(call_site.klass)}) - [:OWNS] -> #{method_node(call_site, "m1")}
+          MERGE (#{query_variable(call_site.klass)}) - [:OWNS] ->
+            #{method_node("m1")}
 
-          MERGE (m1) - [:CONTAINS] -> (cs:CallSite{file: "#{call_site.file}", line_number: #{call_site.line_number}})
+          MERGE (m1) - [:CONTAINS] ->
+            (cs:CallSite
+              {
+                file: {cs_file},
+                line_number: {cs_line_number}
+              }
+            )
 
-          MERGE (#{query_variable(called_code.klass)}) - [:OWNS] -> #{method_node(called_code, "m2")}
+          MERGE (#{query_variable(called_code.klass)}) - [:OWNS] ->
+            #{method_node("m2")}
 
-          MERGE (cs)  - [:CALLS]     ->  m2
+          MERGE (cs) - [:CALLS] -> (m2)
 
           #{args_query args}
         QUERY
@@ -46,16 +76,17 @@ module Delfos
         end
       end
 
-      def method_node(code, id)
-        if code.method_definition_file.length.positive? && code.method_definition_line.positive?
-          <<-NODE
-            (#{id}:Method{type: "#{code.method_type}", name: "#{code.method_name}", file: #{code.method_definition_file.inspect}, line_number: #{code.method_definition_line}})
-          NODE
-        else
-          <<-NODE
-            (#{id}:Method{type: "#{code.method_type}", name: "#{code.method_name}"})
-          NODE
-        end
+      def method_node(id)
+        <<-NODE
+          (#{id}:Method
+            {
+              type: {#{id}_type},
+              name: {#{id}_name},
+              file: {#{id}_file},
+              line_number: {#{id}_line_number}
+            }
+          )
+        NODE
       end
 
       def args_query(args)
@@ -84,7 +115,7 @@ module Delfos
         end
 
         def assign(klass, prefix)
-          klass = klass.to_s.tr(":", "_")
+          klass = klass.to_s
           val = self[klass]
           return val if val
 
--- lib/delfos/neo4j/informer_spec.rb
+++ lib/delfos/neo4j/informer_spec.rb
@@ -18,7 +18,7 @@ describe Delfos::Neo4j::Informer do
     double "CallSite",
       klass: A,
       file: "a.rb",
-      line_number: "4",
+      line_number: 4,
       method_name: "method_a",
       method_type: "ClassMethod",
       method_definition_file: "a.rb",
@@ -29,7 +29,7 @@ describe Delfos::Neo4j::Informer do
     double "CalledCode",
       klass: E,
       file: "e.rb",
-      line_number: "2",
+      line_number: 2,
       method_name: "method_e",
       method_definition_file: "some/filename",
       method_definition_line: 2,
@@ -43,6 +43,7 @@ describe Delfos::Neo4j::Informer do
 
     it do
       result = subject.args_query(args)
+
       expect(result).to eq <<-QUERY.gsub(/^\s+/, "").chomp
         MERGE (cs) - [:ARG] -> (k3)
         MERGE (cs) - [:ARG] -> (k4)
@@ -51,25 +52,73 @@ describe Delfos::Neo4j::Informer do
     end
   end
 
-  it do
+  it "#params_for" do
+    params = subject.params_for(args, call_site, called_code)
+
+    expect(params).to eq({
+      "k1" => "A",
+      "k2" => "E",
+      "k3" => "B",
+      "k4" => "C",
+      "k5" => "D",
+      "m1_type" => "ClassMethod",
+      "m1_name" => "method_a",
+      "m1_file" => "a.rb",
+      "m1_line_number" => 2,
+
+      "cs_file" => "a.rb",
+      "cs_line_number" => 4,
+
+      "m2_type" => "InstanceMethod",
+      "m2_name" => "method_e",
+      "m2_file" => "some/filename",
+      "m2_line_number" => 2,
+    })
+  end
+
+  it "#query_for" do
     query = subject.query_for(args, call_site, called_code)
 
     expected = <<-QUERY
-      MERGE (k1:Class {name: "A"})
-      MERGE (k2:Class {name: "E"})
-      MERGE (k3:Class {name: "B"})
-      MERGE (k4:Class {name: "C"})
-      MERGE (k5:Class {name: "D"})
+      MERGE (k1:Class {name: {k1}})
+      MERGE (k2:Class {name: {k2}})
+      MERGE (k3:Class {name: {k3}})
+      MERGE (k4:Class {name: {k4}})
+      MERGE (k5:Class {name: {k5}})
 
-      MERGE (k1) - [:OWNS] -> (m1:Method{type: "ClassMethod", name: "method_a", file: "a.rb", line_number: 2})
+      MERGE (k1) - [:OWNS] ->
+        (m1:Method
+          {
+            type: {m1_type},
+            name: {m1_name},
+            file: {m1_file},
+            line_number: {m1_line_number}
+         }
+       )
 
-      MERGE (m1) - [:CONTAINS] -> (cs:CallSite{file: "a.rb", line_number: 4})
+      MERGE (m1) - [:CONTAINS] ->
+        (cs:CallSite
+          {
+            file: {cs_file},
+            line_number: {cs_line_number}
+          }
+        )
 
-      MERGE (k2) - [:OWNS] -> (m2:Method{type: "InstanceMethod", name: "method_e", file: "some/filename", line_number: 2})
-      MERGE (cs) - [:CALLS] -> m2
-      MERGE (cs) - [:ARG] -> (k3)
-      MERGE (cs) - [:ARG] -> (k4)
-      MERGE (cs) - [:ARG] -> (k5)
+      MERGE (k2) - [:OWNS] ->
+        (m2:Method
+          {
+            type: {m2_type},
+            name: {m2_name},
+            file: {m2_file},
+            line_number: {m2_line_number}
+         }
+       )
+
+      MERGE (cs) - [:CALLS] -> (m2)
+
+      MERGE (cs) - [:ARG]   -> (k3)
+      MERGE (cs) - [:ARG]   -> (k4)
+      MERGE (cs) - [:ARG]   -> (k5)
     QUERY
 
     expect(strip_whitespace(query)).to eq strip_whitespace(expected)
