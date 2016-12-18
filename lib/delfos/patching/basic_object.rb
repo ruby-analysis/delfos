5c907ea3066aa2f4ae145ddf91fb34b25a8637af
Mark Burns <markthedeveloper@gmail.com>
2016-12-18 12:50:57 +0000

    fix typo

--- lib/delfos/patching/basic_object.rb
+++ lib/delfos/patching/basic_object.rb
@@ -7,7 +7,7 @@ class BasicObject
   end
 
   def self.singleton_method_added(name)
-    return if %i(defined_method extended included inherited method_added singleton_method_added).include?(name)
+    return if %i(define_method extended included inherited method_added singleton_method_added).include?(name)
 
     ::Delfos::Patching::MethodOverride.setup(self, name, private_methods, class_method: true)
   end
