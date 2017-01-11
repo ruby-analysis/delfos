b14bad443da54b13d20593063ee08fa5ff78dfe4
Mark Burns <markthedeveloper@gmail.com>
2017-01-11 17:14:21 +0000

    move files

---
+++ lib/delfos/patching/parameters/method_finder.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+require "parser/current" unless defined?(Parser::CurrentRuby)
+
+require_relative "determine_constant"
+require_relative "argument_rewriter"
+
+module Delfos
+  module Patching
+    module Parameters
+      class MethodFinder
+        include DetermineConstant
+
+        def initialize(filename)
+          @filename = filename
+        end
+
+        attr_reader :filename
+
+        def args_from(method, type)
+          sexp = find(method.name)
+          return "" unless sexp
+
+          args = sexp.children[1]
+
+          args = args.children.
+            select { |a| a.type == type }.
+            map(&:children)
+
+          args.each_with_object({}) do |(name, val), result|
+            result[name] = rewrite_constants(val, method)
+          end
+        end
+
+        def find(m, sexp = file_sexp)
+          return unless sexp.is_a?(Parser::AST::Node)
+
+          return sexp if (sexp.type == :def) && (node_name(sexp) == m.to_s)
+
+          sexp.children.each do |s|
+            next unless s
+            result = find(m, s)
+            return result if result
+          end
+
+          nil
+        end
+
+        def node_name(sexp)
+          snippet_from(sexp.loc.name)
+        end
+
+        private
+
+        def methods(sexp, found = [])
+          if sexp.type == :def
+            found << sexp
+            found
+          end
+        end
+
+        def rewrite_constants(val, method)
+          ArgumentRewriter.new(snippet_from(val.loc.expression), method.owner).perform
+        end
+
+        def snippet_from(o)
+          start = o.begin_pos
+          finish = o.end_pos
+
+          snippet(start, finish)
+        end
+
+        def snippet(start, finish)
+          file_contents[start..finish - 1]
+        end
+
+        def file_contents
+          @file_contents ||= Pathname.new(filename).read
+        end
+
+        def file_sexp
+          Parser::CurrentRuby.parse(file_contents)
+        end
+      end
+    end
+  end
+end
