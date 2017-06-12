e432b589af816806b07be2541213c9b6292d38cd
Mark Burns <markthedeveloper@gmail.com>
2017-06-12 17:15:34 +0900

    add support for changing log level with ENV variable

--- spec/support/logging.rb
+++ spec/support/logging.rb
@@ -2,6 +2,6 @@
 require "logger"
 
 $delfos_test_logger = Logger.new(STDOUT).tap do |l|
-  l.level = Logger::ERROR
+  l.level = ENV["LOG_LEVEL"] ? ENV["LOG_LEVEL"].to_i : Logger::ERROR
   l.progname = "Delfos test"
 end
