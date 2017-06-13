c65b4c06cc601b6b429fe36654ae3124b4b3e86f
Mark Burns <markthedeveloper@gmail.com>
2017-06-13 10:11:29 +0900

    remove unused fixtures

--- fixtures/evaluator.rb
+++
@@ -1,9 +0,0 @@
-class InsideFixturesFolder
-  def self.evaluate(ruby=nil, &block)
-    if block_given?
-      self.instance_eval(&block)
-    else
-      self.instance_eval ruby, __FILE__, __LINE__
-    end
-  end
-end
