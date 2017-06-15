48797f5c9bb1d715f7c3996ba86aadb4cb1cadb0
Mazin Power <mazinpower@notonthehighstreet.com>
2017-06-15 16:46:43 +0100

    Add module example to test app

--- examples/small/app/product.rb
+++ examples/small/app/product.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
+module ProductAttributes
+  def self.included(klass)
+    klass.class_eval do
+      attr_reader :price
+    end
+  end
+end
 
 class Product
+  include ProductAttributes
+
   def initialize(price = nil, name = nil)
     @price = price
     @name = name
